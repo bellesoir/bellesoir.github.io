7637519724b81ac4ffc2f77b0ad321b9ca509c5f
Valerie Berrios <vberrios314@gmail.com>
2017-09-12 20:29:33 -0400

    Updated

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
-Cookies.set('name', 'Val');
+
+//Get value of input
+//save it in a cookie
+Cookies.set('name', document.getElementById('user').value);
 
 var fName = Cookies.get('name');
 console.log(fName);
-console.log('Updated');
+
+document.getElementById('welcome').innerText = 'Welcome, ' + fName;
 //document.getElementById('welcome').innerText = '';
