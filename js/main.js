a1d4f4401f02d0fb685328383b82aa6576e12f84
Valerie Berrios <vberrios314@gmail.com>
2017-09-12 20:53:55 -0400

    updated button

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 
 //Get value of input
 //save it in a cookie
-Cookies.set('name', document.getElementById('user').value);
 
-var fName = Cookies.get('name');
 //console.log(fName);
 
 var userButton = document.getElementById('user-btn');
 
 userButton.addEventListener('click', function(){
-    alert('button clicked');
+    alert('button clicked AGAIN!');
+    
+    Cookies.set('name', document.getElementById('user').value);
+    var fName = Cookies.get('name');
     //Update h2 with username
     document.getElementById('welcome').innerHTML = 'Welcome, ' + fName;
 });
