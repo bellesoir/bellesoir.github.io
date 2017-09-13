80ca90894541f03511c70c3f5b234f846c1aee71
Valerie Berrios <vberrios314@gmail.com>
2017-09-12 21:07:07 -0400

    Remembers User

--- js/main.js
+++ js/main.js
@@ -5,12 +5,18 @@
 //console.log(fName);
 
 var userButton = document.getElementById('user-btn');
+function checkCookie(){
+    var cookieName = Cookies.get('name');
+    if (cookieName !== ''){
+        document.getElementById('welcome').innerHTML = 'Welcome, ' + Cookies.get('name');
+    }
+};
+
+checkCookie();
 
 userButton.addEventListener('click', function(){
     alert('button clicked AGAIN!');
-    
     Cookies.set('name', document.getElementById('user').value);
-    var fName = Cookies.get('name');
     //Update h2 with username
-    document.getElementById('welcome').innerHTML = 'Welcome, ' + fName;
+    document.getElementById('welcome').innerHTML = 'Welcome, ' + Cookies.get('name');
 });
